978984e73961f5cb2a8f7f3490d8f710c2be063a
Francisco Remiro <fremigom@myuax.com>
2026-02-23 17:49:00 +0100

    Clase Sala creada

---
+++ src/Sala.java
@@ -0,0 +1,31 @@
+public class Sala {
+
+    private final int codigoSala;
+    private final String descripcion;
+
+    /**
+     * Constructor de la Sala
+     * @param codigoSala identificador de la sala
+     * @param descripcion de la sala
+     */
+    public Sala(int codigoSala, String descripcion) {
+        this.codigoSala = codigoSala;
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigoSala() {
+        return codigoSala;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String toString() {
+        return "Sala{ codigoSala=" + codigoSala + ", descripcion=" + descripcion + "}";
+    }
+
+
+
+
+}
